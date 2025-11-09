8d838eb40ac5232b2e5cb11e8c6fb0ae21be6c35
Ljubov Morozova <ljubov.morozova@askend.com>
2025-11-09 15:13:18 +0200

    CI pipeline config

---
+++ tests/emptyArray.spec.ts
@@ -0,0 +1,19 @@
+import {test, expect, APIRequestContext} from '@playwright/test';
+import {UserDTO} from "./DTO/UserDTO";
+import {StatusCodes} from "http-status-codes";
+
+const baseURLWithEndpoint: string = 'http://localhost:3000/users';
+const prepareEnv = async (request: APIRequestContext): Promise<void> => {
+    const responseUsers = await request.get(`${baseURLWithEndpoint}`);
+    const users: UserDTO[] = await responseUsers.json();
+    for (let i=0; i<users.length; i++) {
+        await request.delete(`${baseURLWithEndpoint}/${users[i].id}`);
+    }
+};
+test('all users: should return empty array when no users', async ({ request }) => {
+    await prepareEnv(request);
+    const response = await request.get(`${baseURLWithEndpoint}`);
+    expect(response.status()).toBe(StatusCodes.OK)
+    const responseBody = await response.text()
+    expect(responseBody).toBe('[]');
+});
